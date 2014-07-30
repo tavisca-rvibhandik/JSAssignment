67445f318b4dd3578bab9c1f0c8f9dd62eb4ef8f
rashmi <rvibhandik@tavisca.com>
2014-07-30 14:42:23 +0530

    Changes to RSS FEED done

--- AJAX/rss.js
+++ AJAX/rss.js
@@ -1,17 +1,11 @@
 window.onload = function () {
-
-    var btnhotel = document.getElementById("hotelid");
-    var btncar = document.getElementById("carid");
-    var btnreload = document.getElementById("reloadid");
-
-
-    btnreload.onclick = function () {
-        window.location.reload();
-    }
-
-
-    btnhotel.onclick = function () {
-        var xmlhttp;
+    document.getElementById('url').addEventListener('keypress', getData);
+}
+function getData() {
+    var keyCode = event.keyCode || event.which;
+    if(keyCode ==13)
+    {
+    	var xmlhttp;
         if (window.XMLHttpRequest) {
             xmlhttp = new XMLHttpRequest();
         }
@@ -23,109 +17,61 @@ window.onload = function () {
             if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
                 //document.getElementById("myDiv").innerHTML=xmlhttp.responseText;
                 var obj = JSON.parse(xmlhttp.responseText);
+
+		var main = document.getElementById("main");
+
+		
+		
+		var inner = document.getElementById("inner");
+				
+		inner.className += 'format-inner';
+
+		   var title = document.getElementById("title");
+			
+
+		        title.innerHTML = "<p class=\"text-style3\">" + obj.responseData.feed.title + "<br>"
+				+ "<p class=\"text-style1\">" + obj.responseData.feed.description + "<br>";	
+
+		        inner.appendChild(title);	
 
                    var i = 0;
+			
+                   while (obj.responseData.feed.entries[i] != 'undefined') {
+                  
 
-                while (obj.deals[i] != 'undefined') {
-                    var div = document.createElement("div");
-                    div.id = i.toString();
-                    if (i % 2 == 0) {
-                        div.className += 'format-div-odd';
-                    }
-                    else
-                        div.className += 'format-div-even';
+			
+
+		           var Div = document.createElement("Div");
+			
+                           //Div.id = i.toString();
+                           if (i % 2 == 0) {
+                                Div.className += 'format-div-odd text-style2';
+                           }
+                    	   else{
+                           	Div.className += 'format-div-even text-style2';
+			   }
 
 
-                    document.body.appendChild(div);
-                    div.innerHTML = div.innerHTML = "HotelId :  " + obj.deals[i].hotelId + "<br>"
-												+ "HotelName :  " + obj.deals[i].hotelName + "<br>"
-												+ "Title : " + obj.deals[i].title + "<br>"
-												+ "Description  :" + obj.deals[i].description + "<br>";
+                    
+                            Div.innerHTML = "<div>" + "<a href=\"\" class=\"format-title\">" + obj.responseData.feed.entries[i].title + "</a>"
+							+ "<p class=\"format-date\">" + obj.responseData.feed.entries[i].publishedDate + "</p></div>"
+							+ "<br>" + obj.responseData.feed.entries[i].contentSnippet + "<br>";
 												
-                    show_image(obj.deals[i].imageUrl, 300, 200,i);
-                    i++;
-                }
+                    		
+                           i++;
+			   inner.appendChild(Div);
+		
+                   }
+		   main.appendChild(inner);
             }
         }
-        function show_image(src, width, height,i) {
-            var n = i.toString();
-            var m = document.getElementById(n);
-            var img = document.createElement("img");
-            img.src = src;
-            img.width = width;
-            img.height = height;
-            img.className += 'image';
-            m.appendChild(img);
-            //document.body.appendChild(img);
-        }
+      
         
-        xmlhttp.open("GET", "http://dev-mystique.tavisca.com/api/deals/all?token=ym5gqryaupna30rugnosdint&$filter=Type eq 'hotel'", true);
+        xmlhttp.open("GET", "http://googlefeed.appacitive.com/?q="+document.getElementById('url').value, true);
 
 
         xmlhttp.send();
+   
     }
 
-
-    //------car-------
-
-    btncar.onclick = function () {
-
-        var xmlhttp;
-        if (window.XMLHttpRequest) {
-            xmlhttp = new XMLHttpRequest();
-        }
-        else {
-            xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
-        }
-        xmlhttp.onreadystatechange = function () {
-
-            if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-                //document.getElementById("myDiv").innerHTML=xmlhttp.responseText;
-                var obj = JSON.parse(xmlhttp.responseText);
-
-                var i = 0;
-
-                while (obj.deals[i] != 'undefined') {
-                    var div = document.createElement("div");
-                    div.id = i.toString();
-                    if (i % 2 == 0) {
-                        div.className += 'format-div-odd';
-                    }
-                    else
-                        div.className += 'format-div-even';
-
-
-                    document.body.appendChild(div);
-                    div.innerHTML = "Rental CompanyName : " + obj.deals[i].rentalCompanyName + "<br>"
-                                                        + "Rental CompanyCode :  " + obj.deals[i].rentalCompanyCode + "<br>"
-                                                        + "Title : " + obj.deals[i].title + "<br>"
-                                                        + "Description  :" + obj.deals[i].description + "<br>";
-                                                       
-                    show_image(obj.deals[i].imageUrl, 300, 200,i);
-                    i++;
-                }
-            }
-        }
-        function show_image(src, width, height,i) {
-            var n = i.toString();
-            var m = document.getElementById(n);
-            var img = document.createElement("img");
-            img.src = src;
-            img.width = width;
-            img.height = height;
-            img.className += 'image';
-            m.appendChild(img);
-            //document.body.appendChild(img);
-        }
-
-               
-        
-        xmlhttp.open("GET", "http://dev-mystique.tavisca.com/api/deals/all?token=ym5gqryaupna30rugnosdint&$filter=Type eq 'car'", true);
-        
-
-        xmlhttp.send();
-    }
-
-
-
-}//window end
+}
